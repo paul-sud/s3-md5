4526616111af63e084f4d839403253761c18a382
Paul Sud <paulsud@stanford.edu>
2021-05-12 19:32:51 -0700

    add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
 use bytes::BytesMut;
-use hex::encode;
+use hex;
 use md5::{Digest, Md5};
 use rusoto_core::region::Region;
 use rusoto_s3::{GetObjectRequest, S3Client, S3};
+use structopt::StructOpt;
 use tokio::io::AsyncReadExt;
+use url::Url;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "s3-md5", about = "Util to calculate md5sums of s3 objects")]
+struct Args {
+    /// S3 URI to compute md5sum, should take form of s3://foo/bar/baz.qux
+    #[structopt()]
+    s3_uri: String,
+}
+
+struct S3Uri {
+    bucket: String,
+    key: String,
+}
+
+impl S3Uri {
+    fn from_string(uri: &str) -> Self {
+        let parsed = Url::parse(uri).unwrap();
+        return Self {
+            bucket: parsed.host_str().unwrap().to_string(),
+            key: parsed.path().trim_start_matches("/").to_string(),
+        };
+    }
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Args::from_args();
+    let s3_uri = S3Uri::from_string(&args.s3_uri);
     // Adapted from
     // https://github.com/rusoto/rusoto/blob/master/integration_tests/tests/s3.rs
     let client = S3Client::new(Region::UsEast2);
     let get_req = GetObjectRequest {
-        bucket: "encode-test-processing".to_string(),
-        key: "caper_out/wgbs/cf5bf980-1a2d-4095-a6af-bd55fb3da2d0/call-make_conf/script"
-            .to_string(),
+        bucket: s3_uri.bucket,
+        key: s3_uri.key,
         ..Default::default()
     };
 
@@ -35,5 +61,5 @@ async fn main() {
         hasher.update(&buffer)
     }
     let final_hash = hasher.finalize();
-    println!("{}", encode(final_hash));
+    println!("{}", hex::encode(final_hash));
 }
